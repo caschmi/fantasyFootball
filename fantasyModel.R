4143207f957531536cd9a61f177460395371ec18
ergeis <ergeis@umich.edu>
2023-11-30 14:04:58 -0500

    model with centered age and log tranformation and squared terms and interaction terms(do we want this?)

--- fantasyModel.R
+++ fantasyModel.R
@@ -5,9 +5,12 @@ fantasy = read.csv("fantasyFootball.csv")
 
 fantasy$occassion = fantasy$Year - 2016
 
+fantasy$Agesquared <- (fantasy$Age)^2
+fantasy$AgeKnotsquared <- (fantasy$AgeKnot.Centered)^2
+
 
 #change correlation structure -- need to change data frame to include "occasion"
-modelFantasy = lme(Performance ~ Age + AgeKnot + Rk + FantPos + Division, 
+modelFantasy = lme(log(Performance + 5.3) ~ Age.Centered + Agesquared + AgeKnot.Centered + AgeKnotsquared + Rk + FantPos + Division + Age.Centered*FantPos + AgeKnot.Centered*FantPos, 
                     data = fantasy, 
                     random = ~1 + Age | PlayerID) 
 
