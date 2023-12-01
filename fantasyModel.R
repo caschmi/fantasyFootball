3de3c1b27b26ad651414dffa9fc0ca37f8719885
Caitlin Seibel <cseibel@umich.edu>
2023-12-01 16:15:39 -0500

    add unadjusted model

--- fantasyModel.R
+++ fantasyModel.R
@@ -7,14 +7,22 @@ fantasy$occassion = fantasy$Year - 2016
 
 fantasy$Agesquared <- (fantasy$Age)^2
 fantasy$AgeKnotsquared <- (fantasy$AgeKnot.Centered)^2
+fantasy$logPerformance <- log(fantasy$Performance + 5.3)
 
+#first analysis: all players, unadjusted model
+model1 <- lme(logPerformance ~ Age.Centered + Agesquared + AgeKnot.Centered + AgeKnotsquared,
+              data = fantasy,
+              random = ~1 + Age | PlayerID)
+summary(model1)
 
+#second analysis: all players, adjusted model
 #change correlation structure -- need to change data frame to include "occasion"
-modelFantasy = lme(log(Performance + 5.3) ~ Age.Centered + Agesquared + AgeKnot.Centered + AgeKnotsquared + Rk + FantPos + Division + Age.Centered*FantPos + AgeKnot.Centered*FantPos, 
+modelFantasy = lme(logPerformance ~ Age.Centered + Agesquared + AgeKnot.Centered + AgeKnotsquared + Rk + FantPos + Division + Age.Centered*FantPos + AgeKnot.Centered*FantPos, 
                     data = fantasy, 
                     random = ~1 + Age | PlayerID) 
 
 summary(modelFantasy)
+anova(modelFantasy)
 
 
 # Data set creation of only individuals who have ages before and after 28
